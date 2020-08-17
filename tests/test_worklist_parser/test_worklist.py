fed3b74dd76219e0dbcdc5057458197a13efad85
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-08-17 21:23:17 +0100

    Updated worklist tests.

--- tests/test_worklist_parser/test_worklist.py
+++ tests/test_worklist_parser/test_worklist.py
@@ -10,9 +10,11 @@ import pytest
 
 # this package
 from mh_utils.utils import camel_to_snake
+from mh_utils.worklist_parser import read_worklist
 from mh_utils.worklist_parser.classes import Checksum, JobData, Worklist
 from mh_utils.worklist_parser.columns import columns
 from tests.test_worklist_parser.test_checksum import FakeChecksumElement
+from tests.test_worklist_parser.test_integration import worklist_file
 from tests.test_worklist_parser.test_job_data import FakeJobDataElement
 from tests.test_worklist_parser.test_parser import FakeParamsElement
 
@@ -114,7 +116,6 @@ class FakeWorklistElement:
 
 
 def test_locked_run_mode():
-
 	element = FakeWorklistElement()
 
 	element.WorklistInfo.LockedRunMode = -1
@@ -137,7 +138,6 @@ def test_locked_run_mode():
 
 
 def test_from_xml():
-
 	element = FakeWorklistElement()
 
 	data = Worklist.from_xml(element)
@@ -255,3 +255,88 @@ def test_as_dataframe(data: Worklist):
 
 	assert df.columns.tolist() == [col for col in columns]
 	assert df.empty
+
+
+def test_as_dataframe_with_data():
+	assert worklist_file.is_file()
+	worklist = read_worklist(worklist_file)
+
+	df = worklist.as_dataframe()
+	print(df)
+	assert isinstance(df, pandas.DataFrame)
+
+	# TODO: test with actual data
+
+	assert df.columns.tolist() == [
+			"Sample ID",
+			"Sample Name",
+			"Rack Code",
+			"Rack Position",
+			"Plate Code",
+			"Plate Position",
+			"Sample Position",
+			"Method",
+			"Override DA Method",
+			"Data File",
+			"Sample Type",
+			"Method Type",
+			"Balance Override",
+			"Inj Vol (µl)",
+			"Equilib Time (min)",
+			"Dilution",
+			"Wt/Vol",
+			"Comment",
+			"Barcode",
+			"Reserved1",
+			"Reserved2",
+			"Reserved3",
+			"Reserved4",
+			"Reserved5",
+			"Reserved6",
+			"Level Name",
+			"Sample Group",
+			"Info.",
+			"Drying Gas",
+			"Gas Temp",
+			"Nebulizer",
+			]
+	assert not df.empty
+
+	assert not df.iloc[0]["Sample ID"]
+	assert df.iloc[0]["Sample Name"] == "Methanol Blank +ve"
+	assert not df.iloc[0]["Rack Code"]
+	assert not df.iloc[0]["Rack Position"]
+	assert df.iloc[0]["Plate Code"] == "PlateOrVial"
+	assert not df.iloc[0]["Plate Position"]
+	assert df.iloc[0]["Sample Position"] == "P2-A3"
+	assert df.iloc[0]["Method"] == PureWindowsPath(
+			r"D:\MassHunter\Methods\Dominic Davis-Foster\Maitre Gunshot Residue Positive.m"
+			)
+	assert not df.iloc[0]["Override DA Method"]
+	assert df.iloc[0]["Data File"] == PureWindowsPath(
+			r"D:\MassHunter\Data\Dominic Davis-Foster\Methanol_Blank_+ve_191121-0001-r001.d"
+			)
+	assert df.iloc[0]["Sample Type"] == "Sample"
+	assert df.iloc[0]["Method Type"] == "Method No Override"
+	assert df.iloc[0]["Balance Override"] == "No Override"
+	assert df.iloc[0]["Inj Vol (µl)"] == "As Method"
+	assert df.iloc[0]["Equilib Time (min)"] == 0
+	assert df.iloc[0]["Dilution"] == 1
+	assert df.iloc[0]["Wt/Vol"] == 0
+	assert not df.iloc[0]["Comment"]
+	assert not df.iloc[0]["Barcode"]
+	assert not df.iloc[0]["Reserved1"]
+	assert not df.iloc[0]["Reserved2"]
+	assert not df.iloc[0]["Reserved3"]
+	assert df.iloc[0]["Reserved4"] == 0
+	assert df.iloc[0]["Reserved5"] == 0
+	assert df.iloc[0]["Reserved6"] == 0
+	assert not df.iloc[0]["Level Name"]
+	assert not df.iloc[0]["Sample Group"]
+	assert not df.iloc[0]["Info."]
+	assert not df.iloc[0]["Drying Gas"]
+	assert not df.iloc[0]["Gas Temp"]
+	assert not df.iloc[0]["Nebulizer"]
+
+	assert df.iloc[11]["Sample Name"] == "Propellant 1ug gas 200"
+	assert df.iloc[11]["Gas Temp"] == "200"

92cd53f867aa3129c70ced80428576fc02fe2d23
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-17 20:47:37 +0100

    Updated configuration files.

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
 		description='Utilities for handing ancillary files produced by MassHunter.',
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-
 		)

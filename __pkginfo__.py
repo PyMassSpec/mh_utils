d613f8d5efa77685ef89bc885c53929eb8af3bca
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-22 17:59:11 +0100

    Updated config files.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -17,22 +17,9 @@ import pathlib
 __all__ = [
 		"__copyright__",
 		"__version__",
-		"modname",
-		"pypi_name",
-		"__license__",
-		"__author__",
-		"short_desc",
-		"author",
-		"author_email",
-		"github_username",
-		"web",
-		"github_url",
 		"repo_root",
 		"install_requires",
 		"extras_require",
-		"project_urls",
-
-		"import_name",
 		]
 
 __copyright__ = """
@@ -40,23 +27,7 @@ __copyright__ = """
 """
 
 __version__ = "0.0.3"
-modname = "mh_utils"
-pypi_name = "mh_utils"
-import_name = "mh_utils"
-__license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-short_desc = 'Utilities for handing ancillary files produced by MassHunter.'
-__author__ = author = 'Dominic Davis-Foster'
-author_email = 'dominic@davis-foster.co.uk'
-github_username = "domdfcoding"
-web = github_url = "https://github.com/domdfcoding/mh_utils"
+
 repo_root = pathlib.Path(__file__).parent
 install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
 extras_require = {'all': []}
-
-
-
-project_urls = {
-		"Documentation": "https://mh_utils.readthedocs.io",
-		"Issue Tracker": f"{github_url}/issues",
-		"Source Code": github_url,
-		}

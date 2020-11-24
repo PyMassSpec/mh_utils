b2fed8db3333e91b73e750cce7a7ac7b87d7bdd1
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-24 09:40:37 +0000

    Increase rerun interval for test_make_pcdl_csv

--- tests/test_pcdl.py
+++ tests/test_pcdl.py
@@ -231,7 +231,7 @@ target_compounds = [
 		]
 
 
-@pytest.mark.flaky(reruns=2, reruns_delay=60)
+@pytest.mark.flaky(reruns=5, reruns_delay=120)
 def test_make_pcdl_csv(
 		tmp_pathplus,
 		file_regression: FileRegressionFixture,
